59ef6aca2a062e3ce24ee8d332eec2bb660b59e3
Dennis Fabri <info@dennisfabri.de>
2022-04-19 20:23:06 +0200

    Fixed constructor

--- records-api/src/main/java/de/dlrg/lifesavingsports/records/api/RecordDto.java
+++ records-api/src/main/java/de/dlrg/lifesavingsports/records/api/RecordDto.java
@@ -1,24 +1,22 @@
 package de.dlrg.lifesavingsports.records.api;
 
-import de.dlrg.lifesavingsports.records.api.Gender;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Value;
-import lombok.extern.jackson.Jacksonized;
 
 import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class RecordDto {
-    private  String acronym;
-    private  String agegroup;
-    private  Gender gender;
-    private  String discipline;
-    private  String name;
-    private  String club;
-    private  String nation;
-    private  Duration time;
-    private  String venue;
-    private  LocalDate date;
+    private String acronym;
+    private String agegroup;
+    private Gender gender;
+    private String discipline;
+    private String name;
+    private String club;
+    private String nation;
+    private Duration time;
+    private String venue;
+    private LocalDate date;
 }
